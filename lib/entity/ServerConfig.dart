f9e263030f6ce442e0f888320726303fd1213335
Hansol Lee <giantsol64@gmail.com>
2020-03-15 21:40:07 +0900

    Configure loopback ip address and port

--- lib/entity/ServerConfig.dart
+++ lib/entity/ServerConfig.dart
@@ -1,7 +1,7 @@
 
 class ServerConfig {
   static const LOOPBACK_IP = '127.0.0.1';
-  static const LOOPBACK_PORT = '0';
+  static const LOOPBACK_PORT = '80';
 
   static const LOOPBACK = ServerConfig(LOOPBACK_IP, LOOPBACK_PORT);
 
